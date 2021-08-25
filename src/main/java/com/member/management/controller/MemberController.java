f167299fd9421c253badf77566abac11ebf1f578
sonyi00 <ksl2950@naver.com>
2021-08-25 22:42:51 +0900

    fix error

--- src/main/java/com/member/management/controller/MemberController.java
+++ src/main/java/com/member/management/controller/MemberController.java
@@ -1,9 +1,28 @@
 package com.member.management.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.member.management.domain.Member;
+import com.member.management.service.MemberManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MemberController {
 
+    @Autowired
+    private MemberManagementService memberManagementService;
+
+    @PostMapping("/members")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createMembers(@RequestBody Member member){
+        memberManagementService.insertMember(member);
+    }
+
+    @GetMapping("/members")
+    public List<Member> getAllMembers(){
+        return memberManagementService.getMembers();
+    }
 
 }
